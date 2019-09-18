4c028d0d89848cab766926b8987b1a3e36d60b3a
rosajulia <36192695+rosajulia@users.noreply.github.com>
2019-09-18 10:40:15 +0200

    Add files via upload

--- dimacs_laden.py
+++ dimacs_laden.py
@@ -1,8 +1,9 @@
 import sys
 import mxklabs.dimacs  
 import argparse
+from itertools import chain
 
-if __name__ == "__main__":
+def main():
   # inladen dicams
   ARGV_LEN = len(sys.argv)
   if ARGV_LEN == 3:
@@ -22,36 +23,118 @@ if __name__ == "__main__":
     else:
       print("usage error")
 
-##### CREATE DICT OF LITERALS #####
-# dict maken waarin je alle mogelijke literals opslaat en een boolean waarde geeft
-dicts = {}
-keys = range(111,1000)
-for i in keys:
-  dicts[i] = "tbd"
+  #### DEFINE LIST OF CLAUSES #####
+  total_input = dimacs1.clauses + dimacs2.clauses
+  literals = list(chain((*total_input))) # alle literals
+  dicts = {i:-1 for i in literals} # -1 betekent unassigned. 1 = true. 0 = false, hierin alle literals opslaan + bool
+  list_true = [] # hierin opslaan welke literals allemaal true zijn
 
-#### DEFINE LIST OF CLAUSES #####
-total_input = dimacs1.clauses + dimacs2.clauses
+  # call unit_clause rule
+  total_input, dicts, list_true = unit_clause(total_input, dicts, list_true)
+  print(list_true)
 
-#### Unit clause rule + tautology rule ####
-for i in total_input:
-  if len(i) == 1:
-    dicts[i[0]] = True
-    for j in [x for x in range(1, 10) if x != abs(i[0]) % 10]:
-      tmp = int((str(i[0])[:2]) + str(j))
-      dicts[tmp] = False
-      
-for i in total_input:
-  if len(i) >= 2:
-    tmp = [abs(j) for j in i]
-    if (len(set(tmp)) != len(i)):
-      dicts[list(set([x for x in tmp if tmp.count(x) > 1]))[0]] = True
-      for j in [x for x in range(1, 10) if x != abs(i[0]) % 10]:
-        tmp = int((str(i[0])[:2]) + str(j))
-        dicts[tmp] = False
+#### SIMPLIFICATION RULES ####
+# unit clause rule
+def unit_clause(total_input, dicts, list_true):
+  for i in total_input:
+    # unit clause: set key in dict op True
+    if len(i) == 1:
+      # create integer from list
+      tmp = i[0]
+      # set on true in dict + add to list_true 
+      dicts[tmp] = True
+      list_true.append(tmp)
+  remove_clause(total_input, list_true)
+  return total_input, dicts, list_true
+
+# remove clauses that are in list_true
+def remove_clause(total_input, list_true):
+  list_empty = []
+  for true_literal in list_true:
+    for clause_list in total_input:
+      for index, clause in enumerate(clause_list):
+        if abs(clause) == true_literal or clause == true_literal:
+          clause_list.remove(clause)
+  return (total_input, list_true)
+
+if __name__ == "__main__":
+    main()
+
+
+
+#print(list_true)
+
+
+#### DAVIS PUTNAM ####
+#### SAT ####
+#if len(total_input) == 0:
+#  print("Sat")
+
+#     # remove from total_input and delete empty set 
+#     i.remove(abs(tmp))
+#     total_input=[e for e in total_input if e]
+#     for i in list_true:
+#       for j in total_input:
+#         for k in j:
+#           if abs(k) == i:
+#             j.remove(k)
+# print(list_true)
+
+# unit clause rule
+#for i in total_input:
+  # unit clause: set key in dict op True
+ # if len(i) == 1:
+    # create integer from list
+  #  tmp = i[0]
+    # set on true in dict + add to list_true 
+   # dicts[tmp] = True
+    #list_true.append(tmp)
+    # remove from total_input and delete empty set (ONLY WITH UNIT CLAUSE)
+    #i.remove(abs(tmp))
+    #total_input=[e for e in total_input if e]
+    #for i in list_true:
+    #  for j in total_input:
+    #    for k in j:
+    #      if abs(k) == i:
+    #        j.remove(k)
+#print(list_true)
+
+
+
+#haal true clauses uit total_input
+
+
+#print(total_input)
+
+# # tautology rule 
+# for i in total_input:
+#   if len(i) >= 2:
+#     tmp = [abs(j) for j in i]
+#     # tautology: set key in dict op True
+#     if (len(set(tmp)) != len(i)):
+#       tmp  = list(set([x for x in tmp if tmp.count(x) > 1]))[0]
+#       dicts[tmp] = True
+#       list_true.append(tmp)
+
+
+
+# print(total_input)  
   
-print(dicts)
+  #for index, value in enumerate(total_input):
+  #  for i in value:
+  #    if abs(i) == key:
+  #      total_input.pop(index)
 
 
+
+
+
+
+
+# # set other literals with same row and column on False --> sudoku specifiek?
+# for j in [x for x in range(1, 10) if x != abs(i[0]) % 10]:
+# #   tmp = int((str(i[0])[:2]) + str(j))
+#   dicts[tmp] = False
 
 #for i in total_input:
 #  if len(i) >= 2:
@@ -77,4 +160,7 @@ print(dicts)
 ######## SPLIT ########
 
 
+#keys = range(111,dimacs1.num_vars+1) # --> het is natuurlijk niet altijd 111, misschien zoeken naar laagste waarde in clauses?
+#for i in keys:
+#  dicts[i] = "tbd" # --> misschien niet van te voren een dict aanmaken, maar tzt literals in dict stoppen
 
